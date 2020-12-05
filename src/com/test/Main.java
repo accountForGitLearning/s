13d63e8ba2983264eb3f847f5304be91de1ff587
n.kibitkin <@n.kibitkin>
2020-12-05 17:46:19 +0700

    Set BPrinter

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,12 +1,13 @@
 package com.test;
 
 import com.test.printers.APrinter;
+import com.test.printers.BPrinter;
 import com.test.printers.Printer;
 
 public class Main {
 
     public static void main(String[] args) {
-        Printer printer = new APrinter();
+        Printer printer = new BPrinter();
         printer.print();
     }
 }
