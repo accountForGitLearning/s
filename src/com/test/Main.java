7e8dd86cc7a57d04bfd2bd64aee860c8e06573d9
n.kibitkin <@n.kibitkin>
2020-12-05 17:48:21 +0700

    J Set EPrinter

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,12 +1,13 @@
 package com.test;
 
-import com.test.printers.APrinter;
+
+import com.test.printers.EPrinter;
 import com.test.printers.Printer;
 
 public class Main {
 
     public static void main(String[] args) {
-        Printer printer = new APrinter();
+        Printer printer = new EPrinter();
         printer.print();
     }
 }
