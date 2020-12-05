6ae1f134bcef2d049175e542291921c5008bf181
n.kibitkin <@n.kibitkin>
2020-12-05 17:17:44 +0700

    C Movable branch

---
+++ src/com/test/movable/Movable.java
@@ -0,0 +1,5 @@
+package com.test.movable;
+
+public interface Movable {
+    public void move();
+}
---
+++ src/com/test/movable/Pawn.java
@@ -0,0 +1,7 @@
+package com.test.movable;
+
+public class Pawn implements Movable {
+    public void move(){
+        System.out.println("It's moved!");
+    }
+}
